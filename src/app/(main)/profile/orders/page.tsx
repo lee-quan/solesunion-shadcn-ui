98f5aeb8d3272928cd68681f1ba9a3d097e693d5
Quan <leequan2000@outlook.com>
2024-04-05 08:43:00 +0800

    remove apolloclient

--- src/app/(main)/profile/orders/page.tsx
+++ src/app/(main)/profile/orders/page.tsx
@@ -1,17 +1,16 @@
 import ProfileOrdersPage from "@/components/pages/profile/ProfileOrdersPage";
-import { makeServerApolloClient } from "@/lib/graphql/client";
 import { GET_ORDER_HISTORY } from "@/lib/graphql/queries/profileQueries";
 import { getServerSession } from "next-auth";
 
 export default async function OrdersPage() {
   const session = await getServerSession();
-  const { data } = await makeServerApolloClient(session).query({
-    query: GET_ORDER_HISTORY,
-    variables: {
-      limit: 10,
-      page: 1,
-    },
-  });
-  console.log(data);
+  // const { data } = await makeServerApolloClient(session).query({
+  //   query: GET_ORDER_HISTORY,
+  //   variables: {
+  //     limit: 10,
+  //     page: 1,
+  //   },
+  // });
+  // console.log(data);
   return <ProfileOrdersPage />;
 }
