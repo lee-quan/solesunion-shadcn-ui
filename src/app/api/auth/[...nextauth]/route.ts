97c5abc7d3fa18cac4f501de5e835bec6eeae9fb
Quan <leequan2000@outlook.com>
2024-04-13 02:47:58 +0800

    original user

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,177 +1,36 @@
-import NextAuth from "next-auth";
-import { NextAuthOptions } from "next-auth";
+import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
 
-// const authOptions: NextAuthOptions = {
-//   debug: true,
-//   session: {},
-//   logger: {
-//     error: console.error,
-//     warn: console.warn,
-//     debug: console.log,
-//     info: console.info,
-//   },
-//   providers: [
-//     CredentialsProvider({
-//       name: "Email and Password",
-//       credentials: {
-//         email: { label: "Email", type: "email" },
-//         username: { label: "Username", type: "text" },
-//         password: { label: "Password", type: "password" },
-//       },
-//       async authorize(credentials) {
-//         return {
-//           id: "1",
-//           name: "Lee Quan-|-405|v8UtMeI2tzXF6Qdgpy7oo5iKY8wxcjjT6LpNsV6Y71962a52",
-//           accessToken: "405|v8UtMeI2tzXF6Qdgpy7oo5iKY8wxcjjT6LpNsV6Y71962a52",
-//           account: {
-//             provider: "email",
-//             type: "credentials",
-//           },
-//           email: "",
-//         };
-//         // let headers = null;
-//         // const res = await fetch(
-//         //   `${process.env.FRONTEND_URL}/api/sanctum/csrf-token`
-//         // );
-//         // if (res.ok) {
-//         //   const header = await res.json();
-//         //   headers = new Headers({
-//         //     ...header,
-//         //   });
-//         // }
-//         // const data = {
-//         //   email: credentials?.email,
-//         //   password: credentials?.password,
-//         // };
-//         // const options = {
-//         //   method: "POST",
-//         //   headers,
-//         //   body: JSON.stringify(data),
-//         // };
-
-//         // try {
-//         //   const response = await fetch(
-//         //     "https://dev-api.solesunion.com/auth/login",
-//         //     {
-//         //       method: "POST",
-//         //       headers: {
-//         //         "Content-Type": "application/json",
-//         //       },
-//         //       body: JSON.stringify(data),
-//         //     }
-//         //   );
-//         //   if (response.ok) {
-//         //     const responseData = await response.json();
-
-//         //     const user = {
-//         //       id: responseData.user.id,
-//         //       name: `${responseData.user.name}-|-${responseData.access_token}`,
-//         //       email: responseData.user.email,
-//         //       accessToken: responseData.access_token,
-//         //       account: {
-//         //         provider: "email",
-//         //         type: "credentials",
-//         //       },
-//         //     };
-//         //     return {
-//         //       ...user,
-//         //       accessToken: responseData.access_token,
-//         //       account: {
-//         //         provider: "email",
-//         //         type: "credentials",
-//         //       },
-//         //     };
-//         //   } else {
-//         //     throw new Error("HTTP error! Status:" + response.status);
-//         //   }
-//         // } catch (error) {
-//         //   throw new Error("HTTP error! Status:" + error);
-//         // }
-//       },
-//     }),
-//   ],
-//   secret: "OM2aNLORR46RW/N8GE6+EdbY4u3dnF3eLENopihbUvo=",
-//   callbacks: {
-//     async jwt({
-//       token,
-//       user,
-//       account,
-//       profile,
-//     }: {
-//       token: any;
-//       account: any;
-//       user: any;
-//       profile?: any;
-//     }) {
-//       if (user) {
-//         token.user = user;
-//         token.accessToken = user.accessToken;
-//         token.role = user.role;
-//       }
-//       return token;
-//     },
-//     async session({ session, token }: { session: any; token: any }) {
-//       session.accessToken = token.accessToken;
-//       session.user = token.user;
-//       session.role = token.role;
-//       return session;
-//     },
-//   },
-// };
-
 const handler = NextAuth({
-  debug: true,
-  session: {},
   secret: process.env.NEXTAUTH_SECRET,
   providers: [
     CredentialsProvider({
-      name: "Email and Password",
+      // The name to display on the sign in form (e.g. "Sign in with...")
+      name: "Credentials",
+      // `credentials` is used to generate a form on the sign in page.
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
       credentials: {
-        email: { label: "Email", type: "email", placeholder: "Your Email" },
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
         password: { label: "Password", type: "password" },
       },
-      async authorize(credentials) {
-        return {
-          id: "1",
-          name: "Lee Quan-|-405|v8UtMeI2tzXF6Qdgpy7oo5iKY8wxcjjT6LpNsV6Y71962a52",
-          accessToken: "405|v8UtMeI2tzXF6Qdgpy7oo5iKY8wxcjjT6LpNsV6Y71962a52",
-          account: {
-            provider: "email",
-            type: "credentials",
-          },
-          role: "A",
-          email: "leequan2000@outlook.com",
-        };
+      async authorize(credentials, req) {
+        // Add logic here to look up the user from the credentials supplied
+        const user = { id: "1", name: "J Smith", email: "jsmith@example.com" };
+
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user;
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null;
+
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
       },
     }),
   ],
-  callbacks: {
-    async jwt({
-      token,
-      user,
-      account,
-      profile,
-    }: {
-      token: any;
-      account: any;
-      user: any;
-      profile?: any;
-    }) {
-      if (user) {
-        token.user = user;
-        token.accessToken = user.accessToken;
-        token.role = user.role;
-      }
-      return token;
-    },
-    async session({ session, token }: { session: any; token: any }) {
-      session.accessToken = token.accessToken;
-      session.user = token.user;
-      session.role = token.role;
-      return session;
-    },
-  },
 });
 
 export { handler as GET, handler as POST };
