6b64828048d4dfb1ba47d900802d98f8a94960e2
Quan <leequan2000@outlook.com>
2024-04-13 17:06:38 +0800

    fix naming

--- src/app/test/auth/page.tsx
+++ src/app/test/auth/page.tsx
@@ -1,5 +1,5 @@
 import { auth } from "@/auth";
-import TestAuthPage from "./testAuthPage";
+import TestAuthPage from "./TestAuthPage";
 
 export default async function Page() {
   const session = await auth();
