37c753a2d71bd132d2b5af149583ca48bb0c6939
Quan <leequan2000@outlook.com>
2024-04-13 22:53:25 +0800

    hooks

---
+++ src/hooks/useMutation.tsx
@@ -0,0 +1,38 @@
+import { useToast } from "@/components/ui/use-toast";
+import { useMutation as useApolloMutation } from "@apollo/client";
+
+interface UseMutationOptions {
+    showToast?: boolean;
+    onSuccess?: () => void;
+}
+export default function useMutation(
+    MUTATION: any,
+    { showToast = true, onSuccess }: UseMutationOptions = { showToast: true }
+) {
+    const { toast } = useToast();
+    return useApolloMutation(MUTATION, {
+        onError: (error) => {
+            if (showToast) {
+                toast({
+                    title: "Error",
+                    description: error.message,
+                    variant: "destructive",
+                });
+            }
+        },
+        onCompleted: (data) => {
+            let value: any = Object.values(data)[0];
+            let success = value.success;
+            if (showToast) {
+                toast({
+                    title: success ? "Success" : "Error",
+                    description: value.message,
+                    variant: success ? "default" : "destructive",
+                });
+            }
+            if (success) {
+                onSuccess && onSuccess();
+            }
+        },
+    });
+}
