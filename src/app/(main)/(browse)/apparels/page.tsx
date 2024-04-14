d7b1101001cb9ab780d2d5f513bec51485ca10b0
Quan <leequan2000@outlook.com>
2024-04-14 23:02:45 +0800

    fix browse page

--- src/app/(main)/(browse)/apparels/page.tsx
+++ src/app/(main)/(browse)/apparels/page.tsx
@@ -8,23 +8,24 @@ import { getClient } from "@/lib/graphql/apollo-client";
 export default async function Page({ searchParams }: { searchParams: any }) {
   const headersList = headers();
   const fullUrl = headersList.get("x-url") || "";
-  const pathname = `/${fullUrl.split("/").pop()}`;
+  const pathname = `${fullUrl.split("/").pop()}`;
 
   const qParam = searchParams.q
     ? JSON.parse(decrypt(searchParams.q))
     : { sizes: [], brands: [], sortBy: "po.created_at desc", page: 1 };
 
+  console.log("HAHASHSAHDSAHDHASDHSAHDSHD", pathname);
   const client = getClient();
   const { data } = await client.query({
     query: GET_PRODUCTS_FOR_BROWSE_PAGE,
     variables: {
-      category: pathname.split("/").pop() || "",
+      category: pathname.includes("?") ? pathname.split("?")[0] : pathname,
       brands: qParam.brands || [],
       sizes: qParam.sizes || [],
       page: qParam.page || 1,
       sortBy: qParam.sortBy || "po.created_at desc",
     },
-    pollInterval: 0,
+    // pollInterval: 0,
   });
 
   return (
