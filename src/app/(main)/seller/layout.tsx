c2b2f2f6515dab9c189c45c3402711078c26fff9
Quan <leequan2000@outlook.com>
2024-05-06 01:15:11 +0800

    pending

--- src/app/(main)/seller/layout.tsx
+++ src/app/(main)/seller/layout.tsx
@@ -11,6 +11,7 @@ import {
   DropdownMenuSeparator,
 } from "@/components/ui/dropdown-menu";
 import {
+  ClockIcon,
   HistoryIcon,
   PackageIcon,
   SneakerIcon,
@@ -23,8 +24,9 @@ import useQParam from "@/hooks/useQParams";
 import { ChevronDownIcon, PinLeftIcon } from "@radix-ui/react-icons";
 import { useState } from "react";
 import { usePathname } from "next/navigation";
+import { useUserRole } from "@/hooks/useUser";
 
-const sellerDashboardNavigationLinks = [
+const vendorDashboardNavigationLinks = [
   {
     label: "Active Listings",
     icon: SneakerIcon,
@@ -66,6 +68,18 @@ const sellerDashboardNavigationLinks = [
   // },
 ];
 
+const sellerDashboardNavigationLinks = [
+  {
+    label: "Pending Listings",
+    icon: ClockIcon,
+    status: "pending",
+    href: `/seller/offer/pending`,
+    description:
+      "This tab will show all sneakers currently listed by the seller that are either available online or have been shipped to and are held at Soles Union, GMBB, ready for purchase.",
+  },
+  ...vendorDashboardNavigationLinks,
+];
+
 export default function SellerDashboardLayout({
   children,
 }: {
@@ -73,13 +87,17 @@ export default function SellerDashboardLayout({
 }) {
   const qParams = useQParam();
   const status = qParams.status ? qParams.status : "active";
-  const [open, setOpen] = useState(false);
   const pathname = usePathname();
-
+  const userRole = useUserRole();
+  const links =
+    userRole === "V"
+      ? vendorDashboardNavigationLinks
+      : sellerDashboardNavigationLinks;
   return (
     <div className="lg:grid min-h-screen w-full lg:grid-cols-[180px_1fr]">
-      <SellerDashboardSidebar pathname={pathname} />
-      <SellerDashboardMenu pathname={pathname} status={status} />
+      <SellerDashboardSidebar pathname={pathname} links={links} />
+
+      <SellerDashboardMenu pathname={pathname} status={status} links={links} />
       <main className="flex flex-1 flex-col p-4 md:gap-8 md:p-6">
         {children}
       </main>
@@ -87,7 +105,19 @@ export default function SellerDashboardLayout({
   );
 }
 
-function SellerDashboardSidebar({ pathname }: { pathname: string }) {
+function SellerDashboardSidebar({
+  pathname,
+  links,
+}: {
+  pathname: string;
+  links: {
+    label: string;
+    href: string;
+    icon: any;
+    status: string;
+    description: string;
+  }[];
+}) {
   return (
     <div className="hidden border-r bg-gray-100/40 lg:block dark:bg-gray-800/40">
       <div className="flex h-full max-h-screen flex-col gap-2">
@@ -98,7 +128,7 @@ function SellerDashboardSidebar({ pathname }: { pathname: string }) {
         </div>
         <div className="flex-1 overflow-auto py-2">
           <nav className="grid items-start px-4 text-sm font-medium">
-            <SellerDashboardNavigationItems pathname={pathname} />
+            <SellerDashboardNavigationItems pathname={pathname} links={links} />
           </nav>
         </div>
       </div>
@@ -109,37 +139,53 @@ function SellerDashboardSidebar({ pathname }: { pathname: string }) {
 function SellerDashboardMenu({
   pathname,
   status,
+  links,
 }: {
   pathname: string;
   status: string;
+  links: {
+    label: string;
+    href: string;
+    icon: any;
+    status: string;
+    description: string;
+  }[];
 }) {
   return (
     <header className="lg:hidden flex h-14 items-center gap-4 border-b bg-gray-100/40 px-6">
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button className="lg:hidden border border-gray-200" variant="ghost">
-            {
-              sellerDashboardNavigationLinks.find(
-                (link) => link.status === status
-              )?.label
-            }{" "}
+            {links.find((link) => link.status === status)?.label}{" "}
             <ChevronDownIcon className="h-4 w-4" />
           </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent align="end">
           <DropdownMenuLabel>Seller Dashboard</DropdownMenuLabel>
           <DropdownMenuSeparator />
-          <SellerDashboardNavigationItems pathname={pathname} />
+          <SellerDashboardNavigationItems pathname={pathname} links={links} />
         </DropdownMenuContent>
       </DropdownMenu>
     </header>
   );
 }
 
-function SellerDashboardNavigationItems({ pathname }: { pathname: string }) {
+function SellerDashboardNavigationItems({
+  pathname,
+  links,
+}: {
+  pathname: string;
+  links: {
+    label: string;
+    href: string;
+    icon: any;
+    status: string;
+    description: string;
+  }[];
+}) {
   return (
     <>
-      {sellerDashboardNavigationLinks.map(
+      {links.map(
         (
           link: {
             label: string;
@@ -169,62 +215,6 @@ function SellerDashboardNavigationItems({ pathname }: { pathname: string }) {
       >
         <PinLeftIcon className="h-4 w-4" />
         Back to Profile
-      </Link>
-    </>
-  );
-}
-
-function SellerNavigationItems({ pathname }: { pathname: string }) {
-  return (
-    <>
-      <Link
-        href={"/profile/payout"}
-        className={cn(
-          "flex items-center gap-3 rounded-lg px-3 py-2 transition-all text-gray-500 hover:text-gray-900",
-          pathname === "/profile/payout"
-            ? "bg-gray-100 text-gray-900"
-            : "text-gray-500"
-        )}
-        onClick={() => {}}
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          <path d="M21 8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16Z" />
-          <path d="m3.3 7 8.7 5 8.7-5" />
-          <path d="M12 22V12" />
-        </svg>
-        Payout Information
-      </Link>
-      <Link
-        href={"/seller"}
-        className="flex items-center gap-3 rounded-lg px-3 py-2 transition-all text-gray-500 hover:text-gray-900"
-        onClick={() => {}}
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          <path d="M21 8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16Z" />
-          <path d="m3.3 7 8.7 5 8.7-5" />
-          <path d="M12 22V12" />
-        </svg>
-        Listings
       </Link>
     </>
   );
