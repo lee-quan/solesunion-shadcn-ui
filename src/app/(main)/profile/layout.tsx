933e4fa1edbb9ae4929ba989523c1f3ec0a9d57c
Quan <leequan2000@outlook.com>
2024-05-12 16:01:29 +0800

    fix url

--- src/app/(main)/profile/layout.tsx
+++ src/app/(main)/profile/layout.tsx
@@ -118,7 +118,7 @@ function SellerNavigationItems({ pathname }: { pathname: string }) {
         Payout Information
       </Link>
       <Link
-        href={"/seller"}
+        href={"/dashboard"}
         className="flex items-center gap-3 rounded-lg px-3 py-2 transition-all text-gray-500 hover:text-gray-900"
         onClick={() => {}}
       >
